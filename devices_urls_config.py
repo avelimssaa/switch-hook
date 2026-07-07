6558cb54f66fe6ab584f1b109ca9321d7d38c960
avelimssaa <vadimzentsov65@gmail.com>
2026-07-07 12:51:24 +0700

    transfer-users-in-class

--- devices_urls_config.py
+++ devices_urls_config.py
@@ -1,81 +1,81 @@
 import requests
 from config import HOST, USERS
 
-def get_token(username, password):
-    AUTH_URL = f'{HOST}/api/v1/oauth2/token'
-    responce = requests.post(AUTH_URL, 
-                                 params={
-        'grant_type': 'password',
-        'username': username,
-        'password': password,
-        'client_id': 'android-client'
-    },
-    auth=('android-client', 'password')
-                             )
-    responce.raise_for_status()
-    data = responce.json()
+# def get_token(username, password):
+#     AUTH_URL = f'{HOST}/api/v1/oauth2/token'
+#     responce = requests.post(AUTH_URL, 
+#                                  params={
+#         'grant_type': 'password',
+#         'username': username,
+#         'password': password,
+#         'client_id': 'android-client'
+#     },
+#     auth=('android-client', 'password')
+#                              )
+#     responce.raise_for_status()
+#     data = responce.json()
 
-    token = data['access_token']
+#     token = data['access_token']
 
-    return token
+#     return token
 
 
-def auth_users():
-    for user_key in USERS:
-        user = USERS[user_key]
-        token = get_token(user['username'], user['password'])
-        user['token'] = token
+# def auth_users():
+#     for user_key in USERS:
+#         user = USERS[user_key]
+#         token = get_token(user['username'], user['password'])
+#         user['token'] = token
     
 
-def get_house_ids(user_data):
+# def get_house_ids(user_data):
 
-    HEADERS = {
-     "Content-Type": "application/json",
-     "Authorization": f"Bearer {user_data['token']}"
-    }
-    GET_HOUSE_URL = f'{HOST}/api/v1/houses'
-    responce = requests.get(
-        GET_HOUSE_URL,
-        params={
-            'language' : 'ru'
-        },
-        headers=HEADERS
-    )
+#     HEADERS = {
+#      "Content-Type": "application/json",
+#      "Authorization": f"Bearer {user_data['token']}"
+#     }
+#     GET_HOUSE_URL = f'{HOST}/api/v1/houses'
+#     responce = requests.get(
+#         GET_HOUSE_URL,
+#         params={
+#             'language' : 'ru'
+#         },
+#         headers=HEADERS
+#     )
 
-    responce.raise_for_status()
+#     responce.raise_for_status()
 
-    data = responce.json()
+#     data = responce.json()
 
-    house_ids = [house['id'] for house in data]
+#     house_ids = [house['id'] for house in data]
 
-    user_data['houses'] = house_ids
+#     user_data['houses'] = house_ids
 
 
-def get_devices_urls(user_data):
-    for house in user_data['houses']:
-        GET_DEVICES_URL = f'{HOST}/api/v1/houses/{house}/devices'
+# def get_devices_urls(user_data):
+#     for house in user_data['houses']:
+#         GET_DEVICES_URL = f'{HOST}/api/v1/houses/{house}/devices'
 
-        HEADERS = {
-            "Content-Type": "application/json",
-            "Authorization": f"Bearer {user_data['token']}"
-        }
+#         HEADERS = {
+#             "Content-Type": "application/json",
+#             "Authorization": f"Bearer {user_data['token']}"
+#         }
 
-        responce = requests.get(
-            GET_DEVICES_URL,
-            headers=HEADERS
-        )
+#         responce = requests.get(
+#             GET_DEVICES_URL,
+#             headers=HEADERS
+#         )
 
-        responce.raise_for_status()
+#         responce.raise_for_status()
 
-        data = responce.json()
+#         data = responce.json()
 
-        for device in data:
-            device_id = device['nodeId']
-            controller_id = device['controllerId']
+#         for device in data:
+#             device_id = device['nodeId']
+#             controller_id = device['controllerId']
 
-            device_url = f'{HOST}/api/v1/ctl/{controller_id}/devices/{device_id}'
-            print(f'DEVICE URLS: {device_url}')
-            user_data['devices'].append(device_url)
+#             device_url = f'{HOST}/api/v1/ctl/{controller_id}/devices/{device_id}'
+#             print(f'DEVICE URLS: {device_url}')
+#             user_data['devices'].append(device_url)
 
 
 
---
+++ user.py
@@ -0,0 +1,92 @@
+import requests
+
+
+class User:
+
+    def __init__(self, host, username, password):
+        self.host = host
+        self.username = username
+        self.password = password
+        self.houses = []
+        self.devices = []
+        self.access_token = ""
+        self.refresh_token = ""
+        self.find_devices()
+    
+    def find_devices(self):
+        self.authentication()
+        self.get_house_ids()
+        for house in self.houses:
+            GET_DEVICES_URL = f'{self.host.ip_address}/api/v1/houses/{house}/devices'
+
+            HEADERS = {
+            "Content-Type": "application/json",
+            "Authorization": f"Bearer {self.access_token}"
+            }
+
+            responce = requests.get(
+            GET_DEVICES_URL,
+            headers=HEADERS
+            )
+
+            responce.raise_for_status()
+
+            data = responce.json()
+
+            for device in data:
+                device_id = device['nodeId']
+                controller_id = device['controllerId']
+
+                device_url = f'{self.host.ip_address}/api/v1/ctl/{controller_id}/devices/{device_id}'
+                print(f'DEVICE URLS: {device_url}')
+                self.devices.append(device_url)
+
+    def get_device_ip_address():
+        pass
+
+    def authentication(self):
+        AUTH_URL = f'{self.host.ip_address}/api/v1/oauth2/token'
+        responce = requests.post(AUTH_URL, 
+                                 params={
+        'grant_type': 'password',
+        'username': self.username,
+        'password': self.password,
+        'client_id': 'android-client'
+        },
+        auth=('android-client', 'password')
+                             )
+        responce.raise_for_status()
+        data = responce.json()
+
+        access_token = data['access_token']
+        refresh_token = data['refresh_token']
+        self.access_token = access_token
+        self.refresh_token = refresh_token
+
+    def get_house_ids(self):
+
+        HEADERS = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {self.access_token}"
+        }
+
+        GET_HOUSE_URL = f'{self.host.ip_address}/api/v1/houses'
+        
+        responce = requests.get(
+            GET_HOUSE_URL,
+            params={
+            'language' : 'ru'
+            },
+        headers=HEADERS
+        )
+
+        responce.raise_for_status()
+
+        data = responce.json()
+
+        house_ids = [house['id'] for house in data]
+
+        self.houses = house_ids
+
+    def check_token():
+        pass
